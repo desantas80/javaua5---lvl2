cd0c5fa20b0f849ba45eff3ba8b79c25cbe7560c
Robertas <robertasktls@gmail.com>
2023-10-17 20:05:36 +0300

    [if] if else with integers without comments

--- src/lt/codeacademy/lv2/git/P1GitApp.java
+++ src/lt/codeacademy/lv2/git/P1GitApp.java
@@ -2,6 +2,14 @@ package lt.codeacademy.lv2.git;
 
 public class P1GitApp {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+       primitiveDataTypes();
+       ifExample();
     }
+    private static void ifExample() {
+
+        int result =(5 > 3)? 5 : 3;
+        }
+
+    }
+    private static void primitiveDataTypes() {}
 }
