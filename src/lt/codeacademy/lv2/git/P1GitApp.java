0b708957b18d9508298ee1f766d673bfb82d899d
Robertas <robertasktls@gmail.com>
2023-10-17 18:43:34 +0300

    [if] if else with booleans

--- src/lt/codeacademy/lv2/git/P1GitApp.java
+++ src/lt/codeacademy/lv2/git/P1GitApp.java
@@ -2,6 +2,16 @@ package lt.codeacademy.lv2.git;
 
 public class P1GitApp {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+       primitiveDataTypes();
+       ifExample();
+    }
+    private static void ifExample() {
+        boolean tiesa = (5>3);
+        if (5>3) {
+            tiesa = true;
+        } else {
+            tiesa = false;
+        }
+
     }
 }
